5b925616d12898a4697b85b809174d056a2420bc
Christof Belisle <Christof@jazab.com>
2020-09-22 12:39:01 -0400

    Environment Setup
    Setting Up the Environment. Adding Github and exporting game to the web.

---
+++ AuryonGIT/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    [SerializeField] float runSpeed = 40f;
+    bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+}
